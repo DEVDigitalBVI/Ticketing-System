ca78b05eb2bc24ada2107a3d8532f1a2efb8a049
DEVDigitalBVI <dev.digitalbvi@gmail.com>
2026-08-29 21:28:04 -0400

    Initial Commit

---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by Jamaal Hodge on 8/29/26.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
